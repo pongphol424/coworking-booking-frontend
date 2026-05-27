6a213c2fae96e17f69c5ca8f87158fa139051b83
pongphol424 <pongphol.kimm@gmail.com>
2026-05-27 19:55:38 +0700

    create: Auth.context.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './Components/Navbar'
+import { AuthProvider } from './context/auth.context'
 
 function App() {
 
   return (
     <>
+      <AuthProvider>
         <Navbar/>
         <Outlet/>
+      </AuthProvider>
     </>
   )
 }
---
+++ src/context/auth.context.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react"
+import { api } from "../api/axios"
+
+
+interface AuthContextType{
+    email: string | null
+    login: (email: string) => void
+    logout: () => void
+}
+
+
+const AuthContext = createContext<AuthContextType | null>(null)
+
+export function AuthProvider({children}:{children:ReactNode}){
+    const [email, setEmail] = useState<string | null>(null);
+    useEffect(()=>{
+        const fetch = async()=>{
+            const res = await api.get('/auth/authUser')
+            setEmail(res.data.email)
+        }
+        fetch()
+    },[])
+    const login = (a:string)=>setEmail(a)
+    const logout = async()=> {
+        await api.post('/auth/logout')   
+        setEmail(null)
+    }
+
+
+    return(
+        <>
+        <AuthContext.Provider value={{email,login,logout}}>
+            {children}
+        </AuthContext.Provider>
+        </>
+    )
+}
+
+export function useAuth():AuthContextType {
+  const ctx = useContext(AuthContext)
+  if (!ctx) throw new Error("useAuth must be used within AuthProvider")
+  return ctx
+}
