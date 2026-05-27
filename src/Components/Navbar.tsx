f3eda28f2d6ffd4a1d3a205234205b59d5aee2bf
pongphol424 <pongphol.kimm@gmail.com>
2026-05-27 20:04:54 +0700

    feat: add authenticated user state to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,18 +1,27 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/auth.context";
 
 
 
 
-export function Navbar(){
-
-    return(
+export function Navbar() {
+    const { email, logout } = useAuth()
+    return (
         <>
-        <div style={{display:"flex"}}>
-            <Link to="/"><button>Home</button></Link>
-            <Link to="/register"><button>Register</button></Link>
-            <Link to="/login"><button>Login</button></Link>
-            <Link to="/profile"><button>User</button></Link>
-        </div>
+            <div style={{ display: "flex" }}>
+                <Link to="/"><button>Home</button></Link>
+                {email ? 
+                <>
+                    <Link to="/profile"><button>{email}</button></Link>
+                    <button onClick={logout}>Logout</button>
+                </>
+                :
+                <>
+                    <Link to="/login"><button>Login</button></Link>
+                    <Link to="/register"><button>Register</button></Link>
+                </>
+                }
+            </div>
         </>
     )
 }
