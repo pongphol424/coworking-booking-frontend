a4c2b3f4525f85a9645603020c7b3725906bf77e
pongphol424 <pongphol.kimm@gmail.com>
2026-05-29 18:54:58 +0700

    feat: add login validation error handling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,79 @@
 import { Form, useActionData, useNavigate, type ActionFunctionArgs } from "react-router-dom";
 import { api } from "../api/axios";
 import { useAuth } from "../context/auth.context";
-import { useEffect} from "react";
+import { useEffect } from "react";
+import { AxiosError } from "axios";
+import { LoginSchema} from "../schema/user.schema";
+import { validate } from "../service/validate";
+import { ZodError } from "zod";
+
+interface LoginFormErrors {
+    email?: string[]
+    password?: string[]
+    message?: string
+}
+
+interface ValidateBackendReturn {
+    path: string[]
+    message: string
+}
 
 export async function loginAction({ request }: ActionFunctionArgs) {
     const formData = await request.formData();
+    let loginData: Record<string, FormDataEntryValue> = {};
+    for (const [key, value] of formData.entries()) {
+        loginData[key] = value;
+    };
+
     try {
-        const res = await api.post('/auth/login', formData)
-        return res.data
-    } catch (error) {
-        console.log(error)
-        throw error
+        await validate(LoginSchema, loginData)
+        const res = await api.post('/auth/login', formData);
+        const email: string = res.data.email;
+        return email;
+
+    } catch (error: any) {
+        if (error instanceof AxiosError) {
+            if (error.response?.data) {
+                if (error.response.data.error) {
+                    const errorData: ValidateBackendReturn[] = error.response.data.error
+                    const messageObject = errorData.reduce<Record<string, string[]>>(
+                        (acc, issue) => {
+                            const key = String(issue.path[0]);
+                            acc[key] = [...(acc[key] ?? []), issue.message];
+                            return acc;
+                        }, {});
+                    return messageObject;
+                }
+                if (error.response.data.message) {
+                    const message = error.response.data.message;
+                    return { message };
+                };
+
+            };
+        };
+
+        if (error instanceof ZodError) {
+            const messageObject = error.issues.reduce<Record<string, string[]>>(
+                (acc, issue) => {
+                    const key = String(issue.path[0]);
+                    acc[key] = [...(acc[key] ?? []), issue.message];
+                    return acc;
+                }, {});
+            return messageObject;
+        };
+
+        throw error;
     }
 }
 
 export function Login() {
-    const actionReturn = useActionData()
+    const actionReturn = useActionData<string | LoginFormErrors | null>()
     const navigate = useNavigate()
-    const {login} = useAuth()
+    const { login } = useAuth()
+
     useEffect(() => {
-        if(actionReturn?.email){
-            login(actionReturn.email)
+        if (typeof (actionReturn) === "string") {
+            login(actionReturn)
             navigate('/')
         }
     }, [actionReturn])
@@ -28,18 +81,50 @@ export function Login() {
     return (
         <>
             <div>Login</div>
+
             <Form method="post">
                 <div>
                     <label>
-                        Email: <input name="email" type="email" placeholder="email" required />
+                        Email:
+                        <input
+                            name="email"
+                            type="email"
+                            placeholder="email" required
+                        />
                     </label>
+
+                    {typeof (actionReturn) !== "string" &&
+                        actionReturn?.email &&
+                        <span>{actionReturn.email}</span>
+                    }
+
                     <br />
+
                     <label>
-                        Password: <input name="password" type="password" placeholder="password" required />
+                        Password:
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            required
+                        />
                     </label>
+
+                    {typeof (actionReturn) !== "string" &&
+                        actionReturn?.password &&
+                        <span>{actionReturn.password}</span>
+                    }
                 </div>
+
                 <button type="submit">Login</button>
             </Form>
+
+            <div>
+                {typeof (actionReturn) !== "string" &&
+                    actionReturn?.message &&
+                    <span>{actionReturn.message}</span>
+                }
+            </div>
         </>
     )
 }
