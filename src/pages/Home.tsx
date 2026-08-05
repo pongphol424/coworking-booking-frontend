86c9c8c32a558313867b41dd0f0fdb9565245cd0
pongphol424 <pongphol.kimm@gmail.com>
2026-08-05 20:42:23 +0700

    feat: implement home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react"
+import { api } from "../api/axios"
 
 
-export function Home(){
+interface Roomtype {
+    id: number,
+    name: string,
+    description: string | null,
+    capacity: number,
+    price: number,
+    statusName: string,
+    facilities: string[]
+}
 
-    return(
+export function Home() {
+    const [roomTypes, setRoomTypes] = useState<Roomtype[]>([])
+    useEffect(() => {
+        const fetch = async () => {
+            const response = await api.get('/user/room-types')
+            setRoomTypes(response.data)
+        };
+        fetch();
+    }, []);
+
+    return (
         <>
             <div>Home</div>
+            <div>
+                {roomTypes.map((roomType) =>
+                    <div style={{ border: "1px solid grey", backgroundColor: "purple", padding: 10}}>
+                        <div>RoomType: {roomType.name}</div>
+                        <div>Description: {roomType.description}</div>
+                        <div>Capacity: {roomType.capacity}</div>
+                        <div>Facilities: {roomType.facilities.join(", ")}</div>
+                        <div>Status: {roomType.statusName}</div>
+                        <div>Price: {roomType.price}</div>
+                    </div>
+                )}
+            </div>
         </>
     )
 }
