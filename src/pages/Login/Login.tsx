f641897358f886005dab59939f58802aad9e7e24
pongphol424 <pongphol.kimm@gmail.com>
2026-08-26 21:11:34 +0700

    fix: login function

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -12,40 +12,51 @@ interface LoginFormErrors {
     message?: string
 }
 
+interface LoginActionProps {
+    email: string
+    isAdmin: boolean
+}
+
+
 export function Login() {
-    const actionData = useActionData<string| LoginFormErrors | null>()
+    const actionData = useActionData<LoginActionProps | LoginFormErrors | null>()
     const navigate = useNavigate()
     const { login } = useAuth()
 
     useEffect(() => {
-        if (typeof (actionData) === "string") {
-            login(actionData)
-            navigate('/')
+        if (actionData !== null && actionData !== undefined &&
+            "email" in actionData && "isAdmin" in actionData
+        ) {
+            login(actionData.email, actionData.isAdmin)
+            navigate("/")
         }
-    }, [actionData, login, navigate])
+    }, [actionData])
     return (
         <>
             <FormBox label="Login" method="post">
-                
+
                 <InputField
                     label="email"
                     name="email"
                     type="email"
-                    error={typeof (actionData) !== "string" ? actionData?.email : ""}
-                    required/>
-                
+                    error={actionData !== null && actionData !== undefined &&
+                        "isAdmin" in actionData ? "" : actionData?.email}
+                    required />
+
                 <InputField
                     label="password"
                     name="password"
                     type="password"
-                    error={typeof (actionData) !== "string" ? actionData?.password : ""}
-                    required/>
+                    error={actionData !== null && actionData !== undefined &&
+                        "isAdmin" in actionData ? "" : actionData?.password}
+                    required />
 
                 <Button buttonstyle="submit" type="submit">Login</Button>
 
             </FormBox>
 
-            <BoxEror error={typeof (actionData) !== "string" ? actionData?.message :""}/>
+            <BoxEror error={actionData !== null && actionData !== undefined &&
+                        "isAdmin" in actionData ? "" : actionData?.message} />
         </>
     )
 }
